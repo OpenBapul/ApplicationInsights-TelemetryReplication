73e7f78ccaf430bff0e603a3d07baeabb6d34ea7
Gongdo <gongdo@live.com>
2016-10-08 18:10:18 +0900

    add EventLog and MetricLog

--- src/ApplicationInsights.TelemetryReplication.Logging/AiLogger.cs
+++ src/ApplicationInsights.TelemetryReplication.Logging/AiLogger.cs
@@ -2,6 +2,8 @@
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ApplicationInsights.TelemetryReplication.Logging
 {
@@ -65,26 +67,131 @@ namespace ApplicationInsights.TelemetryReplication.Logging
             var message = formatter(state, exception) ?? "";
             if (exception == null)
             {
-                TrackTrace(logLevel, eventId, message, state);
+                if (state is IEnumerable<KeyValuePair<string, object>>)
+                {
+                    var properties = state as IEnumerable<KeyValuePair<string, object>>;
+                    var logType = properties
+                        .Where(item => item.Key.Equals("LogType", StringComparison.OrdinalIgnoreCase))
+                        .Select(item => item.Value?.ToString() ?? "")
+                        .FirstOrDefault() ?? "";
+                    if (logType.Equals(nameof(MetricLog), StringComparison.OrdinalIgnoreCase))
+                    {
+                        var metric = MetricLog.Create(properties);
+                        if (metric == null)
+                        {
+                            TrackTrace(logLevel, eventId, message, properties);
+                        }
+                        else
+                        {
+                            TrackMetric(logLevel, eventId, message, metric);
+                        }
+                    }
+                    else if (logType.Equals(nameof(EventLog), StringComparison.OrdinalIgnoreCase))
+                    {
+                        var @event = EventLog.Create(properties);
+                        if (@event == null)
+                        {
+                            TrackTrace(logLevel, eventId, message, properties);
+                        }
+                        else
+                        {
+                            TrackEvent(logLevel, eventId, message, @event);
+                        }
+                    }
+                    else
+                    {
+                        TrackTrace(logLevel, eventId, message, properties);
+                    }
+                }
+                else if (state is EventLog)
+                {
+                    TrackEvent(logLevel, eventId, message, state as EventLog);
+                }
+                else if (state is MetricLog)
+                {
+                    TrackMetric(logLevel, eventId, message, state as MetricLog);
+                }
+                else
+                {
+                    TrackTrace(logLevel, eventId, message, Enumerable.Empty<KeyValuePair<string, object>>());
+                }
             }
             else
             {
-                TrackException(eventId, message, state, exception);
+                TrackException(eventId, message, exception, Enumerable.Empty<KeyValuePair<string, object>>());
             }
         }
 
-        private void TrackTrace<TState>(LogLevel logLevel, EventId eventId, string message, TState state)
+        private void TrackTrace(LogLevel logLevel, EventId eventId, string message, IEnumerable<KeyValuePair<string, object>> properties)
         {
             var telemetry = new TraceTelemetry(message, ToSeverityLevel(logLevel));
+            if (properties != null)
+            {
+                foreach (var item in properties)
+                {
+                    telemetry.Properties[item.Key] = item.Value.ToString();
+                }
+            }
             telemetry.Properties["logger"] = categoryName;
             telemetry.Properties["eventName"] = eventId.Name;
             telemetry.Properties["eventId"] = eventId.Id.ToString();
             telemetryClient.TrackTrace(telemetry);
         }
 
-        private void TrackException<TState>(EventId eventId, string message, TState state, Exception exception)
+        private void TrackMetric(LogLevel logLevel, EventId eventId, string message, MetricLog metric)
+        {
+            var telemetry = new MetricTelemetry(metric.Name, metric.Value);
+            telemetry.Count = metric.Count;
+            telemetry.Min = metric.Min;
+            telemetry.Max = metric.Max;
+            telemetry.StandardDeviation = metric.StandardDeviation;
+            if (metric.Properties != null)
+            {
+                foreach (var item in metric.Properties)
+                {
+                    telemetry.Properties[item.Key] = item.Value.ToString();
+                }
+            }
+            telemetry.Properties["logger"] = categoryName;
+            telemetry.Properties["eventName"] = eventId.Name;
+            telemetry.Properties["eventId"] = eventId.Id.ToString();
+            telemetry.Properties["remark"] = metric.Remark;
+            telemetryClient.TrackMetric(telemetry);
+        }
+
+        private void TrackEvent(LogLevel logLevel, EventId eventId, string message, EventLog @event)
+        {
+            var telemetry = new EventTelemetry(@event.Name);
+            if (@event.Properties != null)
+            {
+                foreach (var item in @event.Properties)
+                {
+                    telemetry.Properties[item.Key] = item.Value.ToString();
+                }
+            }
+            if (@event.Metrics != null)
+            {
+                foreach (var item in @event.Metrics)
+                {
+                    telemetry.Metrics[item.Key] = item.Value;
+                }
+            }
+            telemetry.Properties["logger"] = categoryName;
+            telemetry.Properties["eventName"] = eventId.Name;
+            telemetry.Properties["eventId"] = eventId.Id.ToString();
+            telemetryClient.TrackEvent(telemetry);
+        }
+
+        private void TrackException(EventId eventId, string message, Exception exception, IEnumerable<KeyValuePair<string, object>> properties)
         {
             var telemetry = new ExceptionTelemetry(exception);
+            if (properties != null)
+            {
+                foreach (var item in properties)
+                {
+                    telemetry.Properties[item.Key] = item.Value.ToString();
+                }
+            }
             telemetry.Properties["logger"] = categoryName;
             telemetry.Properties["eventName"] = eventId.Name;
             telemetry.Properties["eventId"] = eventId.Id.ToString();
---
+++ src/ApplicationInsights.TelemetryReplication.Logging/EventLog.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApplicationInsights.TelemetryReplication.Logging
+{
+    /// <summary>
+    /// A log represents event.
+    /// </summary>
+    public class EventLog
+    {
+        public EventLog(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            Name = name;
+        }
+
+        /// <summary>
+        /// The name of this event.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The metrics related with the event.
+        /// </summary>
+        public IDictionary<string, double> Metrics { get; } = new Dictionary<string, double>();
+        /// <summary>
+        /// Other properties.
+        /// </summary>
+        public IDictionary<string, object> Properties { get; } = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Convert to dictionary.
+        /// </summary>
+        /// <returns></returns>
+        public IReadOnlyDictionary<string, object> ToDictionary()
+        {
+            var dic = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            foreach (var item in Properties)
+            {
+                dic[item.Key] = item.Value;
+            }
+            foreach (var item in Metrics)
+            {
+                dic[$"Metric.{item.Key}"] = item.Value;
+            }
+            dic["LogType"] = nameof(EventLog);
+            dic[nameof(Name)] = Name;
+            return dic;
+        }
+
+        /// <summary>
+        /// Create a new EventLog from given properties.
+        /// </summary>
+        /// <param name="properties">Key-value properties.</param>
+        /// <remarks>
+        /// Name is mandatory property.
+        /// Any properties that starts with 'Metric.' will be translated to Metrics.
+        /// </remarks>
+        /// <returns>Returns a new EventLog if given property have the Name property, or null otherwise.</returns>
+        public static EventLog Create(IEnumerable<KeyValuePair<string, object>> properties)
+        {
+            if (properties == null)
+            {
+                return null;
+            }
+            if (false == properties.Any(item =>
+                item.Key.Equals("LogType", StringComparison.OrdinalIgnoreCase)
+                && (item.Value?.ToString() ?? "").Equals(nameof(EventLog), StringComparison.OrdinalIgnoreCase)))
+            {
+                return null;
+            }
+            var @event = new EventLog("No name");
+            var dic = properties.ToDictionary(item => item.Key, item => item.Value, StringComparer.OrdinalIgnoreCase);
+            foreach (var item in dic)
+            {
+                if (item.Key.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    @event.Name = item.Value?.ToString() ?? "";
+                }
+                else if (item.Key.StartsWith("Metric.", StringComparison.OrdinalIgnoreCase)
+                    && item.Key.Length > 7)
+                {
+                    double value = 0.0;
+                    if (double.TryParse((item.Value?.ToString() ?? ""), out value))
+                    {
+                        @event.Metrics[item.Key.Substring(7)] = value;
+                    }
+                }
+                else
+                {
+                    @event.Properties[item.Key] = item.Value;
+                }
+            }
+            return @event;
+        }
+    }
+}
